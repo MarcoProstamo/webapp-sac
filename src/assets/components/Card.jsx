90bbaab2f8c8f24e57e684660fa93e908c362393
MarcoProstamo <Marcopros1201@gmail.com>
2025-01-17 11:41:13 +0100

    Add → Style Trips List Page

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -14,26 +14,31 @@ export default function Card({ trip }) {
   return (
     <div className="col">
       <Link to={`/trips/${id}`} className="text-decoration-none">
-        <div className="card h-100">
+        <div className="card h-100 border-0">
           <img src={img} className="card-img-top h-100" alt={destination} />
-          <div className="card-body h-100">
-            <h3 className="card-title text-center mb-3">{destination}</h3>
-            <p className="card-text">
-              <strong>Dep:</strong> {departure_date}
-            </p>
-            <p className="card-text">
-              <strong>Arr:</strong> {arrival_date}
-            </p>
-            <p className="card-text">
-              <strong>Dur:</strong> {duration}
-            </p>
+          <span className="text-center text-bg-secondary fs-1">
+            {destination}
+          </span>
+          <div className="card-body h-50 d-flex flex-column justify-content-center">
+            <div className="card-text d-flex mx-1 fs-5 position-relative">
+              <span className="badge text-bg-success align-self-center py-2">
+                <i className="fa-solid fa-plane-departure"></i> {departure_date}
+              </span>
+              <hr className="border border-warning border-2 opacity-75 w-100" />
+              <span className="badge text-bg-primary align-self-center py-2">
+                {arrival_date} <i className="fa-solid fa-plane-arrival"></i>
+              </span>
+            </div>
+            <p className="card-text"></p>
           </div>
-          <div className="d-flex justify-content-between align-items-end card-body">
-            <span>
-              <strong>Emg:</strong> {emergency_number}
+          <div className="d-flex justify-content-between align-items-end card-body fs-4">
+            <span className="badge text-bg-danger p-2">
+              <i className="fa-solid fa-triangle-exclamation"></i>{" "}
+              {emergency_number}
             </span>
-            <span>
-              <strong>Guests:</strong> {guest_number}
+
+            <span className="badge text-bg-info p-2">
+              <i className="fa-solid fa-user"></i> {guest_number}
             </span>
           </div>
         </div>
--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Card from "../components/Card";
 export default function HomePage() {
   return (
     <div className="container my-5">
-      <div className="row row-cols-sm-1 row-cols-md-3 row-cols-auto g-3">
+      <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
         {trips && trips.map((trip) => <Card key={trip.id} trip={trip} />)}
       </div>
     </div>
