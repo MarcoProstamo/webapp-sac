71c6dbd32abff7a37cd468ff57bb219d35c392ae
MarcoProstamo <Marcopros1201@gmail.com>
2025-01-17 09:46:40 +0100

    Add → Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./assets/pages/HomePage";
+import TripPage from "./assets/pages/TripPage";
+import ContactsPage from "./assets/pages/ContactsPage";
+import NotFoundPage from "./assets/pages/NotFoundPage";
+
 export default function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/trips">
+          <Route index element={<HomePage></HomePage>} />
+          <Route path=":id" element={<TripPage></TripPage>} />
+        </Route>
+        <Route path="/contacts" element={<ContactsPage></ContactsPage>} />
+        <Route path="*" element={<NotFoundPage></NotFoundPage>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
